824900974af8429a3c30ceda0c930bb23349cf9e
Andy Swen <andyswen@Andys-MacBook-Air.local>
2024-02-10 04:36:44 -0500

    add assets folder

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import About from './About';
 import './App.css';
 import Home from './Home';
 import { Link, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <nav>
+    <>
+      <Header>
+
+      </Header>
+      <Nav>
         <Link to='/' className='nav-item'>Home</Link>
         <Link to='/about' className='nav-item'>About</Link>
-      </nav>
+      </Nav>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
